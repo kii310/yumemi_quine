debbb69d86d57d99bc7c019e34860b72a75085ba
kiichi.sato <101162914+kii310@users.noreply.github.com>
2025-10-06 01:48:17 +0900

    Create yumemi_quine.js

---
+++ yumemi_quine.js
@@ -0,0 +1,64 @@
+eval(`
+
+                                                          (f=()=>{ENCODED_AA=                              'eJzNl0uWwyAMBPc+ha/A/S+XRbIIUgu1gtyE1f
+                                                           BpVdlg8ua67p/b8K0cQ                        G1DqeJE2uwa0UoFnz0hzqiis+mDYDs22zqZj1ZmbSN2
+                                                            WcmoVRCwo+xjdZ8rjEp                     3Vc6/zq+VbVCa2vusLLf7HZPg/s3F6MWCXrJnx7PdZMc
+                                                             OZ/rJFhEpPULOdZRkd/                  hU5FNgxxWBD3GPYD1UQD0BPcDUI+XE40+o3UPpKVV+h8K
+                                                               bRnGX6ki3DHTf43q3x               pJRrc9eRWN4NoxOQ+COGlQw+Q/Gx31xO2mMTJA1ToLxfll
+                                                                lELaLrVHWj4xxcGTGa             EdMLzMeoAtPFBXMjafa5o2TxsP2WGMUZIw/y8IjkhvPbV
+                                                                 4bTIRB0niqhQ2x8SyU           zTPBgrEpTBpPX/ryVdoiQZA3BsKUcXR/+2q2CA6WjVdjA
+                                                                  xtZGmuMg1Vj/BjD/h2         sLxrDsSbj71YyBtFnjT14uf4vjNGPg8j4BfdAHu4=';SE
+                                                                   LF='(f='+f.toString
+                                                                    ().split(String.fro
+                                                                     mCharCode(32)).join
+                                                                       ("").split(String.
+                                                                        fromCharCode(10)).                        jo
+                                                                         in("")+')();'+"//"                      ;inde
+                                                                          x=0;console.log("e                    val("+S
+                                                                           tring.fromCharCode                  (96)+requ
+                                                                             ire('zlib').infla                teSync(Buff
+                                                                               er.from(ENCODED_              AA,"base64")
+                                                                                 ).toString().sp           lit('').map(c=
+                                                                                    >c==='1'?(()=         >{index++;retur
+                                                                                                         n(()=>SELF.at((i
+                                                                                                        ndex-1)%SELF.leng
+                                                                                                       th))();})():c).jo
+                                                                                                      in("")+String.from
+                                                                                                     CharCode(96)+".spl
+                                                                                                    it('"+String.fromC
+                                                                                                  harCode(32)+"').joi
+                                                                                                 n('').split('"+Stri
+                                                                                                ng.fromCharCode(92)
+                                                                                               +"n').join(''))");}
+                                                                                              )();//(f=()=>{ENCOD
+                                                                                             ED_AA='eJzNl0uWwyA
+                                                                                              MBPc+ha/A/S+XRbI
+                                                                                               IUgu1gtyE1fBpV
+                                                                                                dlg8ua67p/b8
+                                                                                                 K0cQG1DqeJ
+                                                                                                  E2uwa0Uo
+                                                                                                   Fnz0hz
+                                                                                                    qiis
+                                                                                                      +
+
+
+
+
+
+
+
+                       mDYDs22             zqZj1Zm    bSN2Wc             moV      RCwo+xj            dZ8rjEp      3Vc6/zq+VbVCa2vusLLf     7HZPg/s            3F6MWCX       rJnx7
+                        PdZMcOZ           /rJFhEp     PULOdZ           Rkd/h      U5FNgxxWB        D3GPYD1UQ      D0BPcDUI+XE40+o3UPpK     VV+h8KbRn        GX6ki3DHT      f43q3x
+                         pJRrc9e         RWN4Nox      OQ+COG          lQw+Q/      Gx31xO2mMT      JA1ToLxfll      lELaLrVHWj4xxcGTGaEd     MLzMeoAtPF      BXMjafa5o2     TxsP2WG
+                          MUZIw/y       8IjkhvP       bV4bTI          RB0niq      hQ2x8SyUzTP    BgrEpTBpPX/      ryVdoi                   QZA3BsKUcXR    /+2q2CA6WjV     djAxtZG
+                            muMg1V     j/BjD/h        2sLxrD          sSbj71      YyBtFnjT14uf  4vjNGPg8j4Bf      dAHu4=                   ';SELF='(f='  +f.toString(     ).split
+                               (Str   ing.fro         mCharC          ode(32      )).join("").split(String.f      romCha  rCode(10)).      join("")+')();'+"//";index     =0;cons
+                                     ole.log          ("eval          ("+Str      ing.fr omCharCode(  96)+re      quire( 'zlib').inf       lateSy nc(Buffer.f  rom(EN     CODED_A
+                                   A,"base6           4")).t          oStrin      g().sp  lit('').m   ap(c=>      c==='1'?(()=>{ind        ex++;r  eturn(()=   >SELF.     at((ind
+                                  ex-1)%SE            LF.len          gth))(      );})()   :c).joi    n("")+      String                   .fromC   harCode    (96)+"     .split(
+                                  '"+Stri             ng.from        CharCod      e(32)+    "').j     oin(''      ).spli                   t('"+S    tring     .fromC     harCode
+                                  (92)+"               n').join    (''))");       })();/              /(f=()      =>{ENC                   ODED_A              A='eJz     Nl0uWwy
+                                  AMBPc+               ha/A/S+XRbIIUgu1gtyE       1fBpVd              lg8ua6      7p/b8K0cQG1DqeJE2uwa     0UoFnz              0hzqii     s+mDYDs
+                                  22zqZj                 1ZmbSN2WcmoVRCwo         +xjdZ8              rjEp3V      c6/zq+VbVCa2vusLLf7H     ZPg/s3              F6MWCX     rJnx7Pd
+                                  ZMcOZ/                   rJFhEpPULOdZ           Rkd/hU              5FNgxx      WBD3GPYD1UQD0BPcDUI+     XE40+o              3UPpKV     V+h8KbR
+  `.split(' ').join('').split('\n').join(''))
